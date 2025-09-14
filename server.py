617c4c10941f8354ddb854096360a75383e2c2cd
SIVARAMAN M <65678175+Sivaraman12345@users.noreply.github.com>
2025-09-14 23:07:07 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,10 +7,10 @@ PORT = 8074
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    print(f"✅ Server is listening on {HOST}:{PORT}...")
+    print(f" Server is listening on {HOST}:{PORT}...")
 
     while True:
         conn, addr = s.accept()
         with conn:
-            print(f"➡️ Connected by {addr}")
+            print(f" Connected by {addr}")
             conn.sendall("Thank you for connecting".encode())
