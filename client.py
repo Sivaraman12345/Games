657a76c8d10b620882609d79831b2298c17f355d
Sivaraman12345 <sivaraman.m12345@gmail.com>
2025-09-15 10:38:23 +0530

    Connect server and client

--- client.py
+++ client.py
@@ -3,9 +3,11 @@ import socket
 
 HOST = 'localhost'
 PORT = 8074
+print("client is running")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
+    print(f"connected with server successfully {HOST}:{PORT}")
     data = s.recv(1024)
 
 print(f"Message from server: {data.decode()}")
